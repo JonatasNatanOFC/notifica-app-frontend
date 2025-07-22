514ad6ca9fc30a666f7a6ff450c52dbee3911e8c
jeacd <joao.euller@academico.ifpb.edu.br>
2025-07-22 19:08:41 -0300

    add myNotifications tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -54,6 +54,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="myNotifications"
+        options={{
+          title: 'Minhas notificações',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
